09bed570e8037d6e8fdb0af9e97823e8b1d1df7c
OctaviaL <l.bilmand@gmail.com>
2023-02-02 14:49:01 +0600

    другой вариант(с коммента на пост)

--- test1/models.py
+++ test1/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from test2.models import Post
+
 
 class Comment(models.Model):
     comment = models.TextField()
+    post_comm = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self) -> str:
         return self.comment
